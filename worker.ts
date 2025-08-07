07066da690a4a02deb4637b320c8ac9fe686c1a9
szzdmj <chuanfuyin001@gmail.com>
2025-08-07 16:29:09 +0800

    Update worker.ts

--- worker.ts
+++ worker.ts
@@ -1,15 +1,13 @@
-import { MY_CONTAINER, getRandom } from "@cloudflare/containers";
-
-export class MyContainer extends Container {
-  defaultPort = 80; // Port the container is listening on
-  sleepAfter = "1m"; // Stop the instance if requests not sent for 1 minutes
-}
-
+import { Container, getRandom } from "@cloudflare/containers";
 const INSTANCE_COUNT = 5;
+class Backend extends Container {
+  defaultPort = 80;
+  sleepAfter = "1m";
+}
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-  let container = await getRandom(env.SZ_CONTAINER, INSTANCE_COUNT);
+  let container = await getRandom(env.MY_CONTAINER, INSTANCE_COUNT);
   // Pass the request to the container instance on its default port
   return await container.fetch(request);
   },
