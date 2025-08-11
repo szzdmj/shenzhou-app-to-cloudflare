265325b20be4609dd130fbd375d59ed5504ed734
szzdmj <chuanfuyin001@gmail.com>
2025-08-11 18:52:47 +0800

    Update worker.ts

--- worker.ts
+++ worker.ts
@@ -1,11 +1,5 @@
-try {
-          import { Container, getRandom } from "@cloudflare/containers";
+import { Container, getRandom } from "@cloudflare/containers";
 
-            // Use containers here
-        } catch (error) {
-            console.warn("Could not load @cloudflare/containers:", error);
-            // Handle the case where containers are not available
-        }
 export class SZContainer extends Container {
   defaultPort = 80; // Port the container is listening on
   sleepAfter = "1m"; // Stop the instance if requests not sent for 1 minutes
